b5e1932e604579948841753e9733ba146d3728fb
ClaireSalvato <claire_salvato26@milton.edu>
2023-12-11 13:13:44 -0500

    Created new classes: Bubble and Basic Game App. Made the bubble wrap over the moonlight image.

--- src/BasicGameApp.java
+++ src/BasicGameApp.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferStrategy;
 
-
+//
 
 //*******************************************************************************
 // Class Definition Section
